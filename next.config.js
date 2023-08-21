f7d4ccc4a9a0d54709b60a8d28c759540a0bfdaf
Carlos Mayora Curzio <carlos.mayora.curzio@gmail.com>
2023-08-21 18:01:51 +0200

    Ignore TypeScript build errors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
